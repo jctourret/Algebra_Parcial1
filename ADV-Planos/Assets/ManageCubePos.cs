66b5eeb5b2d32b563a38d45673fa72fb1a63d3c2
jctourret <54895509+jctourret@users.noreply.github.com>
2020-05-14 23:44:47 -0300

    Comiteo de ADV-Planos

---
+++ ADV-Planos/Assets/ManageCubePos.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using plane;
+using CustomMath;
+
+public class ManageCubePos : MonoBehaviour
+{
+    public GameObject wallFront;
+    public GameObject wallBack;
+    public GameObject wallTop;
+    public GameObject wallBottom;
+    public GameObject wallSideR;
+    public GameObject wallSideL;
+    public GameObject cube;
+
+    [SerializeField]
+    bool inWallFront;
+    [SerializeField]
+    bool inWallBack;
+    [SerializeField]
+    bool inWallTop;
+    [SerializeField]
+    bool inWallBottom;
+    [SerializeField]
+    bool inWallSideR;
+    [SerializeField]
+    bool inWallSideL;
+    [SerializeField]
+    bool inRoom;
+
+    Plane wallFrontPlane;
+    Plane wallBackPlane;
+    Plane wallTopPlane;
+    Plane wallBottomPlane;
+    Plane wallSideRPlane;
+    Plane wallSideLPlane;
+    private void Start()
+    {
+        wallFrontPlane = new Plane(Vec3.Back, wallFront.transform.position);
+        wallBackPlane = new Plane(Vec3.Forward, wallBack.transform.position);
+        wallTopPlane = new Plane(Vec3.Down, wallTop.transform.position);
+        wallBottomPlane = new Plane(Vec3.Up, wallBottom.transform.position);
+        wallSideRPlane = new Plane(Vec3.Left, wallSideR.transform.position);
+        wallSideLPlane = new Plane(Vec3.Right, wallSideL.transform.position);
+    }
+
+    void Update()
+    {
+        if (wallFrontPlane.GetSide(cube.transform.position))
+        {
+            inWallFront = true;
+        }
+        else
+        {
+            inWallFront = false;
+        }
+        if (wallBackPlane.GetSide(cube.transform.position))
+        {
+            inWallBack = true;
+        }
+        else
+        {
+            inWallBack = false;
+        }
+        if (wallTopPlane.GetSide(cube.transform.position))
+        {
+            inWallTop = true;
+        }
+        else
+        {
+            inWallTop = false;
+        }
+        if (wallBottomPlane.GetSide(cube.transform.position))
+        {
+            inWallBottom = true;
+        }
+        else
+        {
+            inWallBottom = false;
+        }
+        if (wallSideRPlane.GetSide(cube.transform.position))
+        {
+            inWallSideR = true;
+        }
+        else
+        {
+            inWallSideR = false;
+        }
+        if (wallSideLPlane.GetSide(cube.transform.position))
+        {
+            inWallSideL = true;
+        }
+        else
+        {
+            inWallSideL = false;
+        }
+        if (wallBottomPlane.GetSide(cube.transform.position) &&
+            wallTopPlane.GetSide(cube.transform.position) &&
+            wallFrontPlane.GetSide(cube.transform.position) &&
+            wallBackPlane.GetSide(cube.transform.position) &&
+            wallSideRPlane.GetSide(cube.transform.position) &&
+            wallSideLPlane.GetSide(cube.transform.position))
+        {
+            inRoom = true;
+        }
+        else
+        {
+            inRoom = false;
+        }
+    }
+}
---
+++ ADV-Planos/Assets/Planes.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using CustomMath;
+
+namespace plane
+{
+    public class Planes : MonoBehaviour
+    {
+        public Planes(Vec3 inNormal, Vec3 inPoint) {    
+            {
+                normal = inNormal;
+                distance = -inNormal.x * inPoint.x - inNormal.y * inPoint.y - inNormal.z * inPoint.z;
+            }
+        }
+        public Planes(Vec3 a, Vec3 b, Vec3 c) {
+            Vec3 AB = a - b;
+            Vec3 AC = c - a;
+            normal = Vec3.Cross(AB, AC);
+            distance = Mathf.Abs (-normal.x * a.x - normal.y * a.y - normal.z * a.z);
+        }
+        public Vec3 normal
+        {
+            get { return normal; }
+            set { normal = value; }
+        }
+        public float distance
+        {
+            get {return distance;}
+            set {distance = value;}
+        }
+        public Plane flipped
+        {
+            get
+            {
+                return new Plane(-normal,distance);
+            }
+        }
+        public static Plane Translate(Plane plane, Vec3 translation)
+        {
+            throw new NotImplementedException();
+        }
+        public Vec3 ClosestPointOnPlane(Vec3 point)
+        {
+            throw new NotImplementedException();
+        }
+        public void Flip()
+        {
+            normal = -normal;
+            distance = -distance;
+        }
+        public float GetDistanceToPoint(Vec3 point)
+        {
+            return ((Vec3.Dot(point, normal)+distance) / Vec3.Magnitude(point));
+        }
+        public bool GetSide(Vec3 point)
+        {
+            if (GetDistanceToPoint(point)>=0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool Raycast(Ray ray, out float enter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SameSide(Vec3 inPt0, Vec3 inPt1)
+        {
+            if (GetDistanceToPoint(inPt0) > 0 && GetDistanceToPoint(inPt1) > 0 ||
+                GetDistanceToPoint(inPt0) < 0 && GetDistanceToPoint(inPt1) < 0 ||
+                GetDistanceToPoint(inPt0) == 0 && GetDistanceToPoint(inPt1) == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public void Set3Points(Vec3 a, Vec3 b, Vec3 c)
+        {
+            Vec3 AB = a - b;
+            Vec3 AC = c - a;
+            normal = Vec3.Cross(AB, AC);
+            distance = -normal.x * a.x - normal.y * a.y - normal.z * a.z;
+        }
+        public void SetNormalAndPosition(Vec3 inNormal, Vec3 inPoint)
+        {
+            normal = inNormal;
+            normal.Normalize();
+            distance = -inNormal.x * inPoint.x - inNormal.y * inPoint.y - inNormal.z * inPoint.z;
+        }
+        public override string ToString()
+        {
+            throw new NotImplementedException();
+        }
+        public string ToString(string format)
+        {
+            throw new NotImplementedException();
+        }
+        public void Translate(Vec3 translation)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
